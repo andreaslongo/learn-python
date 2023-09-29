6b0c0b9bbec08da503db69e31b01aadb98ed63fa
Andreas Longo <git@andreaslongo.com>
2023-09-29 17:37:59 +0200

    Add cargo like wrapper for dev workflows

--- container/run.sh
+++ container/run.sh
@@ -8,7 +8,8 @@ readonly local uid=1000
 readonly local gid=1000
 
 # Use --publish <host_port>:<container_port> to enable networking.
-podman container attach "$(basename ${parent_dir})" || podman container run \
+# TODO: for dev: --volume ~/code/template-python:/root/code/template-python:Z,ro \
+podman container attach "$(basename ${parent_dir})" 2>/dev/null || podman container run \
     --interactive \
     --name="$(basename ${parent_dir})" \
     --publish 8002:7878 \
@@ -18,5 +19,6 @@ podman container attach "$(basename ${parent_dir})" || podman container run \
     --user ${uid}:${gid} \
     --userns keep-id:uid=${uid},gid=${gid} \
     --volume "${parent_dir}":/home/appuser/app:Z,rw \
+    --volume ~/code/template-python/cargo.py:/usr/local/bin/cargo.py:Z,ro \
     --workdir /home/appuser/app \
         localhost/"$(basename ${parent_dir})":latest
