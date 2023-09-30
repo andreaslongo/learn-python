fe5ee4f58e177f262d60c1578b04f8b7a112f770
Andreas Longo <git@andreaslongo.com>
2023-09-30 12:19:33 +0200

    Spawn a thread for each request

--- practice_programs/web_server/src/web_server/__main__.py
+++ practice_programs/web_server/src/web_server/__main__.py
@@ -4,58 +4,59 @@ import socket
 from pathlib import Path
 from pprint import pprint
 import time
-
+from threading import Thread
 
 
 def main():
-    # We set socket.SO_REUSEADDR to avoid: "OSError: [Errno 98] Address already
-    # in use" after ending with CTRL+C.
-    with socket.socket(socket.SO_REUSEADDR) as s:
-        s.bind(("0.0.0.0", 7878))
-        s.listen()
-        while True:
-            c, _ = s.accept()
-            print("Connection established!")
-            handle_connection(c)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # We set socket flag "SO_REUSEADDR", in order to prevent
+    # "OSError: [Errno 98] Address already in use".
+    # https://docs.python.org/3/library/socket.html#example
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(("0.0.0.0", 7878))
+    s.listen()
+
+    while True:
+        c, _ = s.accept()
+        print("Connection established!")
+        Thread(target=handle_connection, args=[c]).start()
 
 
-def handle_connection(c):
+def handle_connection(stream):
     # https://docs.python.org/3/library/io.html#module-io
-    rfile = c.makefile(mode="rb", buffering=-1)
-    # print_request(rfile)
+    rfile = stream.makefile(mode="rb", buffering=-1)
     request_line = rfile.readline().decode().strip()
 
-    if request_line == 'GET / HTTP/1.1':
-        status_line = "HTTP/1.1 200 OK"
-        filename = "hello.html"
-    elif request_line == 'GET /sleep HTTP/1.1':
-        time.sleep(5)
-        status_line = "HTTP/1.1 200 OK"
-        filename = "hello.html"
-    else:
-        status_line = "HTTP/1.1 404 NOT FOUND"
-        filename = "404.html"
+    match request_line:
+        case "GET / HTTP/1.1":
+            status_line = "HTTP/1.1 200 OK"
+            filename = "hello.html"
+        case "GET /sleep HTTP/1.1":
+            status_line = "HTTP/1.1 200 OK"
+            filename = "hello.html"
+            time.sleep(5)
+        case _:
+            status_line = "HTTP/1.1 404 NOT FOUND"
+            filename = "404.html"
 
     contents = Path(filename).read_text()
     length = len(contents)
 
-    response = (
-        f"{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}".encode()
-    )
+    response = f"{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}".encode()
 
-    wfile = c.makefile(mode="wb", buffering=0)
+    wfile = stream.makefile(mode="wb", buffering=0)
     wfile.write(response)
 
-    c.shutdown(socket.SHUT_RDWR)
-    c.close()
+    stream.close()
 
 
 def print_request(stream):
+    rfile = stream.makefile(mode="rb", buffering=-1)
     http_request = []
     for line in rfile:
         if line == b"\r\n":
-            c.shutdown(socket.SHUT_RDWR)
-            c.close()
+            stream.shutdown(socket.SHUT_RDWR)
+            stream.close()
         else:
             http_request.append(line.decode().strip())
 
