1cb566f04dd431d48936d1c976f50848f08252bf
Andreas Longo <git@andreaslongo.com>
2024-03-30 23:31:53 +0100

    Apply changes from template

--- container/run.sh
+++ container/run.sh
@@ -8,17 +8,14 @@ readonly local uid=1000
 readonly local gid=1000
 
 # Use --publish <host_port>:<container_port> to enable networking.
-# TODO: for dev: --volume ~/code/template-python:/root/code/template-python:Z,ro \
-podman container attach "$(basename ${parent_dir})" 2>/dev/null || podman container run \
+# Use --rm to remove the container after exit.
+podman container start --attach --interactive "$(basename ${parent_dir})" 2>/dev/null || podman container run \
     --interactive \
     --name="$(basename ${parent_dir})" \
-    --publish 8002:7878 \
     --pull=newer \
-    --rm \
     --tty \
     --user ${uid}:${gid} \
     --userns keep-id:uid=${uid},gid=${gid} \
-    --volume "${parent_dir}":/home/appuser/app:Z,rw \
-    --volume ~/code/template-python/cargo.py:/usr/local/bin/cargo.py:Z,ro \
-    --workdir /home/appuser/app \
+    --volume "${parent_dir}:/home/appuser/$(basename ${parent_dir}):Z,rw" \
+    --workdir "/home/appuser/$(basename ${parent_dir})" \
         localhost/"$(basename ${parent_dir})":latest
